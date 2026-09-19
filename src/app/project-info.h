b6ff95745725a5233d5ccb4c103180c6f9a4a91c
Shannon Smith <infiltratr@yandex.com>
2026-09-19 15:08:32 +1000

    Align Calendar source copyright headers (1/8)

--- src/app/project-info.h
+++ src/app/project-info.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Shannon Smith
+// Copyright (C) 2016-2026 Shannon Smith
 
 #ifndef CALENDAR_PLUS_PROJECT_INFO_H
 #define CALENDAR_PLUS_PROJECT_INFO_H
