d14ab8f601620e96e60abd211a232f05f5cd8537
Shannon Smith <infiltratr@yandex.com>
2026-09-20 12:57:42 +1000

    Update copyright range to 1993-2026

--- src/adapters/clock-glib-adapter.h
+++ src/adapters/clock-glib-adapter.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2000-2026 Shannon Smith
+// Copyright (C) 1993-2026 Shannon Smith
 
 #ifndef CALENDAR_PLUS_CLOCK_GLIB_ADAPTER_H
 #define CALENDAR_PLUS_CLOCK_GLIB_ADAPTER_H
