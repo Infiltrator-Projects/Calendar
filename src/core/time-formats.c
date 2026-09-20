83593d361d5793580e93320a997f97940430b6d3
Shannon Smith <infiltratr@yandex.com>
2026-09-20 12:59:17 +1000

    Update copyright range to 1993-2026

--- src/core/time-formats.c
+++ src/core/time-formats.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2000-2026 Shannon Smith
+// Copyright (C) 1993-2026 Shannon Smith
 
 /*
  * Native clock-provider registry, shared timing primitives and public dispatch.
